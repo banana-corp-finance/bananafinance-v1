bc033cbbec36e0c5da6d5249b7243dd96e14607b
frankiefab100 <ohaegbulamfrank@gmail.com>
2019-06-28 22:14:40 -0700

    BANANA-64696: Add comment for clarity (add)

--- bananafinance-v1-frontend/src/utils/getTokenLogoURL.ts
+++ bananafinance-v1-frontend/src/utils/getTokenLogoURL.ts
@@ -2,3 +2,5 @@ const getTokenLogoURL = (address: string) =>
   `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
 
 export default getTokenLogoURL
+
+// dummy comment
