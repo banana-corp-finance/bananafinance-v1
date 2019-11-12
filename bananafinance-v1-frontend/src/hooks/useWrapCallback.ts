15984ecacbe60578ce4c4a3d1dcda7f492dc14a5
Vinyl-Davyl <okononfuadavid@gmail.com>
2019-11-12 09:52:33 -0800

    BANANA-33828: Adjust spacing (add)

--- bananafinance-v1-frontend/src/hooks/useWrapCallback.ts
+++ bananafinance-v1-frontend/src/hooks/useWrapCallback.ts
@@ -73,3 +73,5 @@ export default function useWrapCallback(
     return NOT_APPLICABLE
   }, [wethContract, chainId, inputCurrency, outputCurrency, inputAmount, balance, addTransaction])
 }
+
+// dummy comment
