2c1d8305b87b947f59ce671e2253122f5b9c806e
showbaba <samwise858@gmail.com>
2020-10-16 22:06:09 -0700

    BANANA-40797: Refactor unused variable (add)

--- bananafinance-v1-frontend/src/config/constants/endpoints.ts
+++ bananafinance-v1-frontend/src/config/constants/endpoints.ts
@@ -5,3 +5,5 @@ export const SNAPSHOT_VOTING_API = process.env.REACT_APP_SNAPSHOT_VOTING_API
 export const SNAPSHOT_BASE_URL = process.env.REACT_APP_SNAPSHOT_BASE_URL
 export const SNAPSHOT_API = `${SNAPSHOT_BASE_URL}/graphql`
 export const SNAPSHOT_HUB_API = `${SNAPSHOT_BASE_URL}/api/message`
+
+// dummy comment
