ae9c50c701155dd732b0276255348d0b6e6e7b76
nonfungibleayo-eth <name.can.be.blank.eth@gmail.com>
2021-07-05 04:12:51 -0700

    BANANA-83064: Improve readability (add)

--- bananafinance-v1-frontend/src/store/actions/products.action.ts
+++ bananafinance-v1-frontend/src/store/actions/products.action.ts
@@ -42,3 +42,5 @@ interface IChangeSelectedProductActionType { type: string, product: IProduct };
 interface IClearSelectedProductActionType { type: string };
 interface ISetModificationStateActionType { type: string, value:  ProductModificationStatus};
 interface IChangeProductAmountType {type: string, id: number, amount: number};
+
+// dummy comment
