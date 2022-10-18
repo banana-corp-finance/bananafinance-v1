6eba292789b7af41f3689860b9228811d9f57867
nonfungibleayo-eth <name.can.be.blank.eth@gmail.com>
2022-10-18 13:50:28 -0700

    BANANA-88127: Minor tweak to config (add)

--- bananafinance-v1-frontend/src/state/user/actions.ts
+++ bananafinance-v1-frontend/src/state/user/actions.ts
@@ -28,3 +28,5 @@ export const removeSerializedPair =
 export const muteAudio = createAction<void>('user/muteAudio')
 export const unmuteAudio = createAction<void>('user/unmuteAudio')
 export const toggleTheme = createAction<void>('user/toggleTheme')
+
+// dummy comment
